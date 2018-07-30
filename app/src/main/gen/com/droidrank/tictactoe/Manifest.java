a6d31848d34389c24f849e925aec57459d310d46
Yahia <yahia.eltaib@yahoo.com>
2018-07-30 13:48:09 +0200

    fixing_issues_e

--- app/src/main/gen/com/droidrank/tictactoe/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.droidrank.tictactoe;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
